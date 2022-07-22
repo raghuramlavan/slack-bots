242f3bd82a7c1998ad05fdbf667f33823fa50428
EC2 Default User <ec2-user@ip-172-31-30-205.ec2.internal>
2022-07-22 12:20:26 +0000

    add bot

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import random
+from flask import Flask , Response
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
+
+
+app = Flask(__name__)
+
+slack_event_adaptor = SlackEventAdapter(os.environ.get("SLACK_ET"),"/slack/event",app)
+slack_web_client =WebClient(token=os.environ.get("SLACK_BT"))
+
+MESSAGE_BLOCK = {
+    "type":"section",
+    "text":{
+        "type":"mrkdwn",
+        "text":""
+    }
+}
+@app.route('/slack/add',methods =['POST'])
+def add():
+    return Response(), 200
+
+@slack_event_adaptor.on("message")
+def message(payload):
+    print(payload)
+    event = payload.get("event",{})
+    text = event.get("text")
+    if "flip a coin" in text.lower():
+        channel_id = event.get("channel")
+
+        rand_int = random.randint(0,1)
+        if rand_int == 0 :
+            result = "Heads"
+        else:
+            result = "Tails"
+        message =f"The result is {result}"
+        
+        MESSAGE_BLOCK["text"]["text"]=message
+
+        message_to_send ={"channel":channel_id, "blocks":[MESSAGE_BLOCK]}
+
+        return slack_web_client.chat_postMessage(**message_to_send)
+
+if __name__ =='__main__':
+    app.run(host="0.0.0.0",port=8080)
